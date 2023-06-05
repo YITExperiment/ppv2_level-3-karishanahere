a81ee3c4fb99bac3df5e121012c63c80f3089aa5
karishanahere <134935021+karishanahere@users.noreply.github.com>
2023-06-05 21:15:39 +0530

    Add files via upload

--- screen_pet.py
+++ screen_pet.py
@@ -1,7 +1,7 @@
 from tkinter import HIDDEN, NORMAL,Tk, Canvas
 def toggle_eyes():
     current_color = c.itemcget(eye_left, 'fill')
-    new_color = c.cody_color if current_color == 'white' else 'white'
+    new_color = c.body_color if current_color == 'white' else 'white'
     current_state = c.itemcget(pupil_left, 'state')
     new_state = NORMAL if current_state == HIDDEN else HIDDEN
     c.itemconfigure(pupil_left, state=new_state)
